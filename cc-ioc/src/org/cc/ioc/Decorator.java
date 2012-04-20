f88171b64370e4305c158bbb05a4c899b7c6c4ab
dixingxing <dixingxing@yeah.net>
2012-04-20 17:36:51 +0800

    javadoc

--- cc-ioc/src/org/cc/ioc/Decorator.java
+++ cc-ioc/src/org/cc/ioc/Decorator.java
@@ -6,10 +6,18 @@
 package org.cc.ioc;
 /**
  * 
+ * <p>扩展型接口，IOC实例化对象后，调用{@link #doDecorator(Object)}方法对实例进行处理。</p>
  * 
  * @author dixingxing	
- * @date Apr 12, 2012
+ * @date Apr 20, 2012
  */
 public interface Decorator {
+	/**
+	 * 
+	 * <p>Ioc实例化的对象</p>
+	 * @param <T>
+	 * @param obj
+	 * @return
+	 */
 	<T> T doDecorator(Object obj );
 }
